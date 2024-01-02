c730251f918162021e5708e16eecd7ea80768d39
Andrew Cade <theonethatownz92@googlemail.com>
2024-01-02 17:27:10 +0000

    Simple Roll Dice

---
+++ RollDice/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'bodacious'
+game 'gta5'
+
+name "RollDice"
+description "RollDice system for FiveM."
+author "Winters"
+version "1.0.0"
+
+client_scripts {
+    'config.lua',
+    'client/RollDice.lua',
+}
+
+server_scripts {
+    'config.lua',
+    'server/RollDice.lua',
+}
+
